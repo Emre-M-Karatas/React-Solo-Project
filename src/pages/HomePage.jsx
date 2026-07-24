37d71b75bc6dfe2892ea11feb506bc5f6770ca0f
Emre-M-Karatas <emre.karatas1@hotmail.com>
2026-07-24 21:00:33 +1000

    Testing searchpage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,49 +6,47 @@ import building from "../assets/building.png";
 const HomePage = () => {
   return (
     <>
-      <body>
-        <header className="nav__bar">
-          <div className="nav__bar--wrapper container">
-            <a href="#" className="img__logo--wrapper">
-              <img src={logo} className="img__logo" alt="Blinker logo" />
-            </a>
-            <div className="nav__link--list">
-              <Link to="#" className="nav__link">
-                Home
-              </Link>
-              <Link to="/search" className="nav__link">
-                Find Your Movie
-              </Link>
-              <button className="nav__link--btn">Contact</button>
-            </div>
+      <header className="nav__bar">
+        <div className="nav__bar--wrapper container">
+          <a href="#" className="img__logo--wrapper">
+            <img src={logo} className="img__logo" alt="Blinker logo" />
+          </a>
+          <div className="nav__link--list">
+            <Link to="#" className="nav__link">
+              Home
+            </Link>
+            <Link to="/search" className="nav__link">
+              Find Your Movie
+            </Link>
+            <button className="nav__link--btn">Contact</button>
           </div>
-        </header>
-        <section id="landing__page">
-          <div className="landing__page--content">
-            <h1 className="landing__page--title fade-up">
-              Australia's most awarded Movie subscription platform
-            </h1>
-            <h3 className="landing__page--sub-title fade-up">
-              Find your favourite movies with{" "}
-              <span className="purple">Blinker</span>
-            </h3>
-          </div>
-          <div className="input__wrapper container fade-up">
-            <input
-              type="text"
-              placeholder="Search by Name"
-              className="search__bar"
-            />
-            <button className="input__btn">
-              <i className="fa-solid fa-magnifying-glass icon-search"></i>
-              <i className="fa-solid fa-spinner icon-spinner"></i>
-            </button>
-          </div>
-          <div className="img__wrapper">
-            <img src={building} className="landing__page--img" alt="" />
-          </div>
-        </section>
-      </body>
+        </div>
+      </header>
+      <section id="landing__page">
+        <div className="landing__page--content">
+          <h1 className="landing__page--title fade-up">
+            Australia's most awarded Movie subscription platform
+          </h1>
+          <h3 className="landing__page--sub-title fade-up">
+            Find your favourite movies with{" "}
+            <span className="purple">Blinker</span>
+          </h3>
+        </div>
+        <div className="input__wrapper container fade-up">
+          <input
+            type="text"
+            placeholder="Search by Name"
+            className="search__bar"
+          />
+          <button className="input__btn">
+            <i className="fa-solid fa-magnifying-glass icon-search"></i>
+            <i className="fa-solid fa-spinner icon-spinner"></i>
+          </button>
+        </div>
+        <div className="img__wrapper">
+          <img src={building} className="landing__page--img" alt="" />
+        </div>
+      </section>
     </>
   );
 };
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/logo.png";
+import building from "../assets/building.png";
+import { Link } from "react-router-dom";
 
 const SearchPage = () => {
   return (
-    <h1>Search page</h1>
-  )
-}
+    <>
+      <section className="hero__bg">
+        <div className="hero__bg--img"></div>
 
-export default SearchPage
+        <header className="nav__bar container">
+          <div className="nav__bar--wrapper">
+            <a href="#" className="img__logo--wrapper">
+              <img src={logo} className="img__logo" alt="Blinker logo" />
+            </a>
+            <div className="nav__link--list">
+              <Link to="#" className="nav__link">
+                Home
+              </Link>
+              <Link to="/search" className="nav__link">
+                Find Your Movie
+              </Link>
+              <button className="nav__link--btn">Contact</button>
+            </div>
+          </div>
+        </header>
+        <section className="content__wrapper container">
+          <h1 className="content__title">Browse Movies</h1>
+          <div className="input__wrapper">
+            <input
+              type="text"
+              className="input__movies"
+              placeholder="Browse by Name or Year"
+            />
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </div>
+        </section>
+      </section>
+
+      <section id="search" className="container">
+        <div className="filter__content--wrapper">
+          <h1 className="filter__title">
+            Search results for <span className="purple"></span>
+          </h1>
+        </div>
+        <select id="filter">
+          <option value="" disabled selected>
+            Sort
+          </option>
+          <option value="A_to_Z">A to Z</option>
+          <option value="Z_to_A">Z to A</option>
+          <option value="Newest_to_Oldest">Newest to Oldest</option>
+          <option value="Oldest_to_Newest">Oldest to Newest</option>
+        </select>
+      </section>
+
+      <section className="loading container">
+        <i className="fa-solid fa-spinner loading__spinner"></i>
+      </section>
+
+      <section className="movies container">
+        <div className="movies__list"></div>
+      </section>
+    </>
+  );
+};
+
+export default SearchPage;
