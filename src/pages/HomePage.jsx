f8776332c4040cd00aa6e77269a705077879b80c
Emre-M-Karatas <emre.karatas1@hotmail.com>
2026-07-24 20:46:29 +1000

    test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,53 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import building from "../assets/building.png";
 
 const HomePage = () => {
-  return;
-  <>
-    <header class="nav__bar">
-      <div class="nav__bar--wrapper container">
-        <a href="#" class="img__logo--wrapper">
-          <img
-            src="./assest/blinker-icon.4f9b2663-Photoroom.png"
-            class="img__logo"
-            alt=""
-          />
-        </a>
-        <div class="nav__link--list">
-          <a href="#" class="nav__link">
-            Home
-          </a>
-          <a href="#" class="nav__link">
-            Find Your Movie
-          </a>
-          <button class="nav__link--btn">Contact</button>
-        </div>
-      </div>
-    </header>
-    <section id="landing__page">
-      <div class="landing__page--content">
-        <h1 class="landing__page--title fade-up">
-          Australia's most awarded Movie subscription platform
-        </h1>
-        <h3 class="landing__page--sub-title fade-up">
-          Find your favourite movies with <span class="purple">Blinker</span>
-        </h3>
-      </div>
-      <div class="input__wrapper container fade-up">
-        <input type="text" placeholder="Search by Name" class="search__bar" />
-        <button class="input__btn">
-          <i class="fa-solid fa-magnifying-glass icon-search"></i>
-          <i class="fa-solid fa-spinner icon-spinner"></i>
-        </button>
-      </div>
-      <div class="img__wrapper">
-        <img
-          src="./assest/building.681ea6bf.png"
-          class="landing__page--img"
-          alt=""
-        />
-      </div>
-    </section>
-  </>;
+  return (
+    <>
+      <body>
+        <header className="nav__bar">
+          <div className="nav__bar--wrapper container">
+            <a href="#" className="img__logo--wrapper">
+              <img src={logo} className="img__logo" alt="Blinker logo" />
+            </a>
+            <div className="nav__link--list">
+              <Link to="#" className="nav__link">
+                Home
+              </Link>
+              <Link to="/search" className="nav__link">
+                Find Your Movie
+              </Link>
+              <button className="nav__link--btn">Contact</button>
+            </div>
+          </div>
+        </header>
+        <section id="landing__page">
+          <div className="landing__page--content">
+            <h1 className="landing__page--title fade-up">
+              Australia's most awarded Movie subscription platform
+            </h1>
+            <h3 className="landing__page--sub-title fade-up">
+              Find your favourite movies with{" "}
+              <span className="purple">Blinker</span>
+            </h3>
+          </div>
+          <div className="input__wrapper container fade-up">
+            <input
+              type="text"
+              placeholder="Search by Name"
+              className="search__bar"
+            />
+            <button className="input__btn">
+              <i className="fa-solid fa-magnifying-glass icon-search"></i>
+              <i className="fa-solid fa-spinner icon-spinner"></i>
+            </button>
+          </div>
+          <div className="img__wrapper">
+            <img src={building} className="landing__page--img" alt="" />
+          </div>
+        </section>
+      </body>
+    </>
+  );
 };
 
 export default HomePage;
